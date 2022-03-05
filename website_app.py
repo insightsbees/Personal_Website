552e4f28b376b14de9cff04b513be37747546777
Insights Bees <insightsbees@gmail.com>
2022-03-05 12:37:34 -0500

    commit

--- website_app.py
+++ website_app.py
@@ -1,20 +1,16 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
-import streamlit.components.v1 as html
+#import streamlit.components.v1 as html
 from  PIL import Image
 import numpy as np
 import pandas as pd
-from st_aggrid import AgGrid
-from raceplotly.plots import barplot
-from collections import deque
-#from link_button import link_button
-import streamlit.components.v1 as components
+#import streamlit.components.v1 as components
 import base64
 
 
 with st.sidebar:
-    choose = option_menu("Main Menu", ["About", "Tutorials","Apps", "Contact"],
-                         icons=['house', 'file-slides','app-indicator','person lines fill'],
+    choose = option_menu("Main Menu", ["About","Resume", "Projects", "Blog","Apps", "Contact"],
+                         icons=['house','file-person', 'bar-chart-line','file-slides','app-indicator','person lines fill'],
                          menu_icon="list", default_index=0,
                          styles={
         "container": {"padding": "5!important", "background-color": "#fafafa"},
@@ -40,7 +36,7 @@ if choose == "About":
     st.write("Sharone Li is a data science practitioner, enthusiast, and blogger. She writes data science articles and tutorials about Python, data visualization, Streamlit, etc. She is also an amateur violinist who loves classical music.\n\nTo read Sharone's data science posts, please visit her Medium blog at: https://medium.com/@insightsbees")    
     st.image(profile, width=700 )
 
-elif choose == "Tutorials": 
+elif choose == "Blog": 
         topic = option_menu(None, ["Streamlit", "Pandas","Plotly", "Folium"],
                          icons=['book', 'book','book','book'],
                          menu_icon="list", default_index=0,
@@ -64,8 +60,9 @@ elif choose == "Tutorials":
         #st.markdown('<p class="font">Featured Posts</p>', unsafe_allow_html=True)  
         #st.write('') 
         if topic=='Pandas':
-            st.subheader('Featured Pandas Tutorials')
+            st.subheader('Latest Pandas Tutorials for Beginners')
             st.write('---')
+
             feature_image1 = Image.open(r'C:\Users\13525\Desktop\Insights Bees\streamlit_website\Images\feature_image1.jpg')
             with st.container():
                 image_col, text_col = st.columns((1,3))
@@ -77,7 +74,7 @@ elif choose == "Tutorials":
                     font-size:22px ; font-family: 'Black'; color: #FFFFF;} 
                     </style> """, unsafe_allow_html=True)
                     st.markdown('<p class="font">Clean a ‘Numeric’ ID Column in Pandas Dataframe</p>', unsafe_allow_html=True)    
-                    st.markdown("As a data scientist, you must have encountered this problem at least once in your data science journey: you import your data into a Pandas dataframe... [Continue to Read on Towards Data Science](https://towardsdatascience.com/clean-a-numeric-id-column-in-pandas-dataframe-fbe03c11e330)")
+                    st.markdown("By Sharone Li - As a data scientist, you must have encountered this problem at least once in your data science journey: you import your data into a Pandas dataframe... [Continue to Read on Towards Data Science](https://towardsdatascience.com/clean-a-numeric-id-column-in-pandas-dataframe-fbe03c11e330)")
 
             col1, col2,col3 = st.columns(3)
             with col1: 
@@ -102,22 +99,59 @@ elif choose == "Tutorials":
                                 data=PDFbyte,
                                 file_name="pandas-clean-id-column.pdf",
                                 mime='application/octet-stream')
-            #st.write('')
-            st.write('---')
-            #st.write('')
 
-            feature_image2 = Image.open(r'C:\Users\13525\Desktop\Insights Bees\streamlit_website\Images\feature_image2.png')
+            st.write('---')
+            feature_image2 = Image.open(r'C:\Users\13525\Desktop\Insights Bees\streamlit_website\Images\feature_image3.png')
             with st.container():
                 image_col, text_col = st.columns((1,3))
                 with image_col:
                     st.image(feature_image2,caption='Image by Pixabay')
 
                 with text_col:
+                    st.markdown(""" <style> .font {
+                    font-size:22px ; font-family: 'Black'; color: #FFFFF;} 
+                    </style> """, unsafe_allow_html=True)
+                    st.markdown('<p class="font">How to Batch Rename Columns in Pandas Based on Patterns</p>', unsafe_allow_html=True)    
+                    st.markdown("By Sharone Li - If you have been following my Medium blog for some time, you may notice that I usually like to share... [Continue to Read on CodeX](https://medium.com/codex/how-to-batch-rename-columns-in-pandas-based-on-patterns-7d2382b5fc9a)")
+                    
+
+            col1, col2,col3 = st.columns(3)
+            with col1: 
+                #st.button('Read PDF Tutorial', key='1')
+ 
+                if st.button('Read PDF Tutorial',key='7'): 
+                    def show_pdf(file_path):
+                        with open(file_path,"rb") as f:
+                            base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+                        pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>'
+                        st.markdown(pdf_display, unsafe_allow_html=True)
+            
+                    show_pdf('post3.pdf')
+            with col2:
+                st.button('Close PDF Tutorial',key='8')                   
+            with col3:
+                # if st.button('Download PDF Tutorial',key='3'):
+                        with open("post3.pdf", "rb") as pdf_file:
+                            PDFbyte = pdf_file.read()
+
+                        st.download_button(label="Download PDF Tutorial",key='9',
+                                data=PDFbyte,
+                                file_name="pandas-rename-columns.pdf",
+                                mime='application/octet-stream')
+
+            st.write('---')
+            feature_image3 = Image.open(r'C:\Users\13525\Desktop\Insights Bees\streamlit_website\Images\feature_image2.png')
+            with st.container():
+                image_col, text_col = st.columns((1,3))
+                with image_col:
+                    st.image(feature_image3,caption='Image by Pixabay')
+
+                with text_col:
                     # st.markdown(""" <style> .font {
                     # font-size:22px ; font-family: 'Black'; color: #FFFFF;} 
                     # </style> """, unsafe_allow_html=True)
                     st.markdown('<p class="font">Why and How to Reshape a Pandas Dataframe from Wide to Long</p>', unsafe_allow_html=True)    
-                    st.markdown("As data scientists, we know that data does not always come to us with the most desirable format... [Continue to Read on Towards Data Science](https://towardsdatascience.com/clean-a-numeric-id-column-in-pandas-dataframe-fbe03c11e330)")
+                    st.markdown("By Sharone Li - As data scientists, we know that data does not always come to us with the most desirable format... [Continue to Read on Towards Data Science](https://towardsdatascience.com/clean-a-numeric-id-column-in-pandas-dataframe-fbe03c11e330)")
                     
                     
 
@@ -132,12 +166,12 @@ elif choose == "Tutorials":
                     #     pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>'
                     #     st.markdown(pdf_display, unsafe_allow_html=True)
             
-                    show_pdf('post3-compressed.pdf')
+                    show_pdf('post1-compressed.pdf')
             with col2:
                 st.button('Close PDF Tutorial',key='5')                   
             with col3:
                 # if st.button('Download PDF Tutorial',key='3'):
-                        with open("post3-compressed.pdf", "rb") as pdf_file:
+                        with open("post1-compressed.pdf", "rb") as pdf_file:
                             PDFbyte = pdf_file.read()
 
                         st.download_button(label="Download PDF Tutorial",key='6',
@@ -146,45 +180,4 @@ elif choose == "Tutorials":
                                 mime='application/octet-stream')
 
 
-            #st.write('')
-            st.write('---')
-            #st.write('')
-
-            feature_image3 = Image.open(r'C:\Users\13525\Desktop\Insights Bees\streamlit_website\Images\feature_image3.png')
-            with st.container():
-                image_col, text_col = st.columns((1,3))
-                with image_col:
-                    st.image(feature_image3,caption='Image by Pixabay')
-
-                with text_col:
-                    st.markdown(""" <style> .font {
-                    font-size:22px ; font-family: 'Black'; color: #FFFFF;} 
-                    </style> """, unsafe_allow_html=True)
-                    st.markdown('<p class="font">How to Batch Rename Columns in Pandas Based on Patterns</p>', unsafe_allow_html=True)    
-                    st.markdown("If you have been following my Medium blog for some time, you may notice that I usually like to share... [Continue to Read on CodeX](https://towardsdatascience.com/clean-a-numeric-id-column-in-pandas-dataframe-fbe03c11e330)")
-                    
-
-            col1, col2,col3 = st.columns(3)
-            with col1: 
-                #st.button('Read PDF Tutorial', key='1')
- 
-                if st.button('Read PDF Tutorial',key='7'): 
-                    def show_pdf(file_path):
-                        with open(file_path,"rb") as f:
-                            base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-                        pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>'
-                        st.markdown(pdf_display, unsafe_allow_html=True)
-            
-                    show_pdf('post1-compressed.pdf')
-            with col2:
-                st.button('Close PDF Tutorial',key='8')                   
-            with col3:
-                # if st.button('Download PDF Tutorial',key='3'):
-                        with open("post1-compressed.pdf", "rb") as pdf_file:
-                            PDFbyte = pdf_file.read()
-
-                        st.download_button(label="Download PDF Tutorial",key='9',
-                                data=PDFbyte,
-                                file_name="tutorial.pdf",
-                                mime='application/octet-stream')
 
